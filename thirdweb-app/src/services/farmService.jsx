520bf40a06adc8b17b805c9658c7926e0d6d7afe
Huy0110 <duchuy01102001@gmail.com>
2023-11-02 02:24:49 +0000

    Add authen logic

--- thirdweb-app/src/services/farmService.jsx
+++ thirdweb-app/src/services/farmService.jsx
@@ -5,7 +5,20 @@ const FARM = {
   me: () => privateHttp({
     method: 'GET',
     url: '/farm/me'
-}),
+  }),
+  login: async ({email, password}) => {
+    let result = await publicHttp({
+        method: 'POST',
+        url: 'api/auth/signin',
+        data: {
+            email,
+            password
+        }
+    });
+
+    console.log("result: ", result)
+    return result;
+  },
 }
 
 export default FARM;
