aca2e2ad96439f1704993d5b87424e61aa9b0ff7
Huy0110 <duchuy01102001@gmail.com>
2023-12-23 08:11:17 +0000

    Add navigate to login

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import CreateProject from './pages/CreateProject'
 import ProjectList from './pages/ProjectList'
 import MainLayout from './components/layout/MainLayout'
@@ -19,16 +19,17 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<LoginPage />} path="/login" />
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route element={<LoginPage />} path="/login" />
         </Route>
-        <Route element={<MainLayout />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<Auth path={'login'}>{<ProjectList />}</Auth>} path="home" />
-          <Route index element={<Auth path={'login'}>{<Profile />}</Auth>} path="profile" />
-          <Route index element={<Auth path={'login'}>{<ManagePlant />}</Auth>} path="manage-plant" />
-          <Route index element={<Auth path={'login'}>{<ManageTemplate />}</Auth>} path="manage-template" />
-          <Route index element={<Auth path={'login'}>{<ManageRequest />}</Auth>} path="manage-request" />
-          <Route index element={<Auth path={'login'}>{<ManageGarden />}</Auth>} path="manage-planting-garden" />
+          <Route element={<Auth path={'login'}>{<Profile />}</Auth>} path="profile" />
+          <Route element={<Auth path={'login'}>{<ManagePlant />}</Auth>} path="manage-plant" />
+          <Route element={<Auth path={'login'}>{<ManageTemplate />}</Auth>} path="manage-template" />
+          <Route element={<Auth path={'login'}>{<ManageRequest />}</Auth>} path="manage-request" />
+          <Route element={<Auth path={'login'}>{<ManageGarden />}</Auth>} path="manage-planting-garden" />
           <Route element={<Auth path={'/login'}>{<GardenProjectDetail />}</Auth>} path="manage-planting-garden/:id" />
           <Route element={<Auth path={'/login'}>{<CreateProject />}</Auth>} path="create-project" />
           <Route element={<Auth path={'/login'}>{<ProjectDetail />}</Auth>} path="project/:id" />
