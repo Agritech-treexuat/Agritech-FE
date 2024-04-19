727e6c5b8885d0ec90f49ebe6ad56360e3d75bf4
Huy0110 <duchuy01102001@gmail.com0>
2024-04-19 17:57:04 +0700

    Update new project

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -9,10 +9,6 @@ import Auth from './hooks/auth'
 import Profile from './pages/Profile'
 import ManagePlant from './pages/ManagePlant'
 import PlantDetail from './pages/PlantDetail'
-import ManageTemplate from './pages/ManageTemplate'
-import ManageRequest from './pages/ManageRequest'
-import ManageGarden from './pages/ManageGarden'
-import GardenProjectDetail from './pages/GardenProjectDetail'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import OtherInfo from './pages/OtherInfo'
 import ResetPasswordPage from './pages/ResetPasswordPage'
@@ -32,10 +28,6 @@ const App = () => {
           <Route index element={<Auth path={'/login'}>{<ProjectList />}</Auth>} path="home" />
           <Route element={<Auth path={'/login'}>{<Profile />}</Auth>} path="profile" />
           <Route element={<Auth path={'/login'}>{<ManagePlant />}</Auth>} path="manage-plant" />
-          <Route element={<Auth path={'/login'}>{<ManageTemplate />}</Auth>} path="manage-template" />
-          <Route element={<Auth path={'/login'}>{<ManageRequest />}</Auth>} path="manage-request" />
-          <Route element={<Auth path={'/login'}>{<ManageGarden />}</Auth>} path="manage-planting-garden" />
-          <Route element={<Auth path={'/login'}>{<GardenProjectDetail />}</Auth>} path="manage-planting-garden/:id" />
           <Route element={<Auth path={'/login'}>{<ProjectDetail />}</Auth>} path="project/:id" />
           <Route element={<Auth path={'/login'}>{<PlantDetail />}</Auth>} path="plant/:id" />
           <Route element={<Auth path={'/login'}>{<OtherInfo />}</Auth>} path="other-information" />
